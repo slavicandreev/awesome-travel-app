9d23d1c1a14a0c0f58541ab34fac0f3f0fbae406
Cameron White <whitcam20@gmail.com>
2021-10-19 21:00:15 -0400

    adding traveller model

--- models/Traveller.js
+++ models/Traveller.js
@@ -7,7 +7,23 @@ class Traveller extends Model {}
 // create fields/columns for Traveller model
 Traveller.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
 
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {isEmail: true}
+    }
   },
   {
     sequelize,
