4ee409b27a21a4960f52af4251440daeffa843cb
Cameron White <whitcam20@gmail.com>
2021-10-19 21:48:18 -0400

    adding get traveller, add, and delete

--- routes/api/travellerRoutes.js
+++ routes/api/travellerRoutes.js
@@ -3,22 +3,62 @@ const { Traveller, Trip, Location } = require('../../models');
 
 // GET all travellers
 router.get('/', async (req, res) => {
-
+    try {
+        const travellerData = await Traveller.findAll()
+        res.status(200).json(travellerData);
+    }
+    catch(error) {
+        res.status(500).json(error);
+    }
+    
 });
 
 // GET a single traveller
 router.get('/:id', async (req, res) => {
-
+    try {
+        const travellerData = await Traveller.findByPk(req.params.id, {
+            include: [{ model:Location, through: Trip, as: 'planned_trips'}]
+        });
+        if (!travellerData) {
+            res.status(404).json({ message: 'No traveller data found'})
+        }
+        res.status(200).json(travellerData);
+    }
+    catch(error) {
+        res.status(500).json(error);
+    }
 });
 
 // CREATE a traveller
 router.post('/', async (req, res) => {
-
+    try {
+        const travellerData = await Traveller.create({
+            traveller_id: req.body.traveller_id,
+        });
+        res.status(200).json(travellerData);
+    }
+    catch(error) {
+        res.status(400).json(error);
+    }
 });
 
 // DELETE a traveller
 router.delete('/:id', async (req, res) => {
-
+    try{
+        const travellerData = await Traveller.destroy({
+            where: {
+                id: req.params.id,
+            }
+        });
+        if (!travellerData) {
+            res.status(404).json({ message: 'No traveller data found'})
+            return;
+        }
+        res.status(200).json(travellerData);
+    }
+    catch (error) {
+        res.status(500).json(error);
+    }
 });
 
 module.exports = router;
