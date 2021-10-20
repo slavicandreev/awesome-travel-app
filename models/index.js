08cb684e153bd781957e7a62d4b0729a535a50e0
Slavic Andreev <writeslavic@gmail.com>
2021-10-19 20:10:02 -0500

    connect

--- models/index.js
+++ models/index.js
@@ -2,8 +2,24 @@ const Traveller = require('./Traveller');
 const Location = require('./Location');
 const Trip = require('./Trip');
 
+Traveller.belongsToMany(Location, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: Trip,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'planned_trips'
+});
 
-Trip.belongsTo(Traveller);
-Trip.belongsTo(Location);
+Location.belongsToMany(Traveller, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: Trip,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'location_travellers'
+});
 
 module.exports = { Traveller, Location, Trip };
